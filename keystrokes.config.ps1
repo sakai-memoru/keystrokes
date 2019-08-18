7c75c2a97700f2b5bc6b7fe182f880fdc036c8b5
mitz <mitsuru.sakai@gmail.com>
2019-08-18 15:48:26 +0900

    to operate for alphabet, numeric, symbols count and convert 'shift + x' into one symbol.

--- keystrokes.config.ps1
+++ keystrokes.config.ps1
@@ -26,3 +26,31 @@ $C_output_path = $C_output_path.Replace("{{date_str}}", $date_str)
 
 ## exclusion list
 $C_exclusion_words = @("Shift","Ctrl","Win","Alt","Left","Right","Up","Down")
+
+## shift keys dictionary for converting
+$C_shift_key_dic = @{}
+$C_shift_key_dic.Add('Shift + 1', '!') #01
+$C_shift_key_dic.Add('Shift + 2', '"') #02
+$C_shift_key_dic.Add('Shift + 3', '#') #03
+$C_shift_key_dic.Add('Shift + 4', '$') #04
+$C_shift_key_dic.Add('Shift + 5', '%') #05
+$C_shift_key_dic.Add('Shift + 6', '&') #06
+$C_shift_key_dic.Add('Shift + 7', "'") #07
+$C_shift_key_dic.Add('Shift + 8', '(') #08
+$C_shift_key_dic.Add('Shift + 9', ')') #09
+$C_shift_key_dic.Add('Shift + -', '=') #10
+$C_shift_key_dic.Add('Shift + ^', '~') #11 FIXME
+$C_shift_key_dic.Add('Shift + @', '`') #12
+$C_shift_key_dic.Add('Shift + [', '{') #13
+$C_shift_key_dic.Add('Shift + ]', '}') #14
+$C_shift_key_dic.Add('Shift + ;', '+') #15
+$C_shift_key_dic.Add('Shift + :', '*') #16
+$C_shift_key_dic.Add('Shift + ,', '<') #17
+$C_shift_key_dic.Add('Shift + .', '>') #18
+$C_shift_key_dic.Add('Shift + /', '?') #19
+$C_shift_key_dic.Add('Shift + \', '_') ##20
+# $C_shift_key_dic
+
+## symbols
+$C_symbols = '!"#$%&''()-=^~@`[{;+:*]},<.>/?\_'
+# $C_symbols
--- keystrokes.ps1
+++ keystrokes.ps1
@@ -3,15 +3,9 @@
 $APIsignaturesWin32 = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
 public static extern short GetAsyncKeyState(int virtualKeyCode); 
-[DllImport("user32.dll", CharSet=CharSet.Auto)]
-public static extern int GetKeyboardState(byte[] keystate);
-[DllImport("user32.dll", CharSet=CharSet.Auto)]
-public static extern int MapVirtualKey(uint uCode, int uMapType);
-[DllImport("user32.dll", CharSet=CharSet.Auto)]
-public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
 '@
 
-$API = Add-Type -MemberDefinition $APIsignaturesWin32 -Name 'Win32' -Namespace API -PassThru
+$Win32 = Add-Type -MemberDefinition $APIsignaturesWin32 -Name 'Win32' -Namespace API -PassThru
 
 $APIsignaturesUtils = @'
 [DllImport("user32.dll")]
@@ -33,15 +27,18 @@ function get-activewin()
   $activewin
 }
 
-function output-line($logPath, $ary_str, $cnt){
+function output-line($logPath, $ary_str, $cnt_alpha, $cnt_numeric, $cnt_symbol, $cnt_mouse, $cnt_other){
   $activewin = get-activewin
   $arylst = New-Object System.Collections.ArrayList
   $null = $arylst.Add((Get-Date).ToString($C_dateformat))
   $null = $arylst.Add($activewin.Id)
-  $null = $arylst.Add($cnt)
+  $null = $arylst.Add($cnt_alpha)
+  $null = $arylst.Add($cnt_numeric)
+  $null = $arylst.Add($cnt_symbol)
+  $null = $arylst.Add($cnt_other)
   $null = $arylst.Add($ary_str)
   $ary = $arylst.ToArray()
-  $line = [String]::Join("`t", $ary)
+  $line = $ary -join "`t"
   
   Add-Content -Path $logPath -Value $line -Encoding $C_Encode
   if($C_debug_mode){
@@ -51,6 +48,36 @@ function output-line($logPath, $ary_str, $cnt){
     $logger.info.Invoke("line=$line")
   }
 }
+
+function convert-value($value){
+  if($value.length -le 1){
+    $rtn = $value
+  }else{
+    if($value.Startswith('Shift + ')){
+      if($C_shift_key_dic.ContainsKey($value)){
+        $rtn = $C_shift_key_dic[$value]
+      }else{
+        $start = 'Shift + '.Length
+        $len = $value.Length
+        $str = $value.Substring($start,$len - $start)
+        $rtn = $str.ToUpper().Trim()
+        
+        ## FIXME When some chars inputting
+        # $tmp_strs = str.Split(",")
+        # $tmp_formatted = $tmp_strs | Foreach-Object {$_.ToUpper()} | Foreach-Object {$_.Trim()}
+        # $rtn = $tmp_formatted -Join ''
+      }
+    }elseif($value -ieq 'space'){
+      $rtn = ' '
+    }else{
+      $rtn = $value
+    }
+  }
+  ## $logger.info.Invoke("value=$value, rtn=$rtn")
+  ## return
+  $rtn
+}
+
 
 ## function
 function Log-Keystrokes($logPath="$env:temp\Keystrokes.txt") 
@@ -63,6 +90,13 @@ function Log-Keystrokes($logPath="$env:temp\Keystrokes.txt")
     $line = "datetime`tprocessid`tcount`tkeystrokes"
     Add-content -Path $logPath  -Value "$line`r`n" -Encoding $C_Encode
   }
+  # variables
+  $cnt_alpha = 0
+  $cnt_numeric = 0
+  $cnt_symbol = 0
+  $cnt_mouse = 0
+  $cnt_other = 0
+  $symbol_ary = $C_symbols -split ""
   # buf
   $arylst = New-Object System.Collections.ArrayList
   $buf = ''
@@ -72,27 +106,34 @@ function Log-Keystrokes($logPath="$env:temp\Keystrokes.txt")
     $logger.warn.Invoke('Keylogger started. Press CTRL+C to see results...')
     $logger.info.Invoke("Output keystroke log to ... $logPath")
     
-    $cnt = 0
+    
     while ($true) {
       Start-Sleep -Milliseconds $C_waittime
       $starttime = Get-Date
       $flag = $true
-      
       while ($flag) {
         for ($btKeyCode = 0; $btKeyCode -le 254; $btKeyCode++) {
           ## FIXME [ ] How can I get WheeelUp and WheelDown,etc.
           
           ## get key state
-          $keystate = $API::GetAsyncKeyState($btKeyCode)
+          $keystate = $Win32::GetAsyncKeyState($btKeyCode)
           
           ## if key pressed
           if ($keystate -eq -32767) {
             $null = [console]::CapsLock ##FIXME Why this line is written?
-            $val = (Get-ItemProperty -path 'HKCU:\Software\GetKeypressValue').KeypressValue
-            
+            $val_org = (Get-ItemProperty -path 'HKCU:\Software\GetKeypressValue').KeypressValue
+            $val = convert-value $val_org
             if($val.length -le 1){
               $buf = $buf + $val
-              $cnt = $cnt + 1
+              if($val -match "[a-zA-Z]"){
+                $cnt_alpha = $cnt_alpha + 1
+              }elseif($val -match "[0-9]"){
+                $cnt_numeric = $cnt_numeric + 1
+              }elseif($symbol_ary.Contains($val)){
+                $cnt_symbol = $cnt_symbol + 1
+              }else{
+                $cnt_other = $cnt_other + 1
+              }
             }else{
               if($buf -ne ""){
                 $null = $arylst.Add($buf)
@@ -100,7 +141,7 @@ function Log-Keystrokes($logPath="$env:temp\Keystrokes.txt")
               if($C_exclusion_words -notcontains $val){
                 if($val.length -ne 0){
                   $null = $arylst.Add($val)
-                  $cnt = $cnt + 1
+                  $cnt_other = $cnt_other + 1
                 }
               }
               $buf = ""
@@ -114,14 +155,18 @@ function Log-Keystrokes($logPath="$env:temp\Keystrokes.txt")
         }
       }
       $ary = $arylst.ToArray()
-      $ary_str = [string]::Join(",",$ary)
+      $ary_str = $ary -join ","
       # if($C_debug_mode){
       #   $logger.info.Invoke("logPath=$logPath")
       #   $logger.info.Invoke("cnt=$cnt")
       #   $logger.info.Invoke("ary_str=$ary_str")
       # }
-      output-line $logPath $ary_str $cnt
-      $cnt = 0
+      output-line $logPath $ary_str $cnt_alpha $cnt_numeric $cnt_symbol $cnt_mouse $cnt_other 
+      $cnt_alpha = 0
+      $cnt_numeric = 0
+      $cnt_symbol = 0
+      $cnt_mouse = 0
+      $cnt_other = 0
       $buf = ''
       $arylst.Clear()
     }
@@ -130,7 +175,7 @@ function Log-Keystrokes($logPath="$env:temp\Keystrokes.txt")
   { 
     $null = $arylst.Add($buf)
     $ary = $arylst.ToArray()
-    $ary_str = [string]::Join(",",$ary)
+    $ary_str = $ary -join ","
     $cnt = $buf.length + $ary.length - 1
     output-line $logPath $ary_str $cnt
   }
